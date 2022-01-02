a0906a4bc437caa684579c42a8dee2bad4bfe5d2
SangwonYoon <yoonsw0532@naver.com>
2022-01-02 16:43:29 +0900

    전자 액자 기능 추가

--- app/src/main/java/com/example/digitalpictureframe/MainActivity.kt
+++ app/src/main/java/com/example/digitalpictureframe/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.digitalpictureframe
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -33,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val imageUriList : MutableList<Uri> = mutableListOf() // 갤러리에서 선택된 사진의 Uri를 저장할 리스트
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,8 +51,7 @@ class MainActivity : AppCompatActivity() {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 when{ // when문에 매개변수를 주지 않으면 if-else과 같이 사용할 수 있다.
                     ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED -> {
-                        // TODO 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
-                        navigatePhotos()
+                        navigatePhotos() // 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
                     }
                     shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> { // 사용자가 권한 요청을 명시적으로 거부한 경우 true를 반환 / 사용자가 권한 요청을 처음 보거나, 다시 묻지 않음 선택한 경우, 권한을 허용한 경우 false를 반환
                         showPermissionContextPopup()
@@ -57,6 +61,17 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
+    }
+
+    private fun initStartPhotoFrameModeButton(){
+        startPhotoFrameModeButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, PhotoFrameActivity::class.java)
+            imageUriList.forEachIndexed { index, uri -> // 리스트에 들어있는 데이터를 인덱스값과 함께 꺼냄
+                intent.putExtra("photo$index", uri.toString())
+            }
+            intent.putExtra("photoListSize", imageUriList.size) // 사진을 몇개 꺼내야하는지 알려주기 위함
+            startActivity(intent)
         }
     }
 
@@ -88,6 +103,36 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, 2000)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode != Activity.RESULT_OK){ // 정상적으로 사진이 선택되지 않은 경우
+            return
+        }
+
+        when(requestCode){
+            2000 -> {
+                val selectedImageUri : Uri? = data?.data // Uri란? 인터넷 자원을 나타내는 고유 식별자(identifier)
+                if(selectedImageUri != null){
+
+                    if(imageUriList.size == 6){
+                        Toast.makeText(this, "사진이 꽉 찼습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri) // ImageView에 선택된 사진을 set
+
+                } else{
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun showPermissionContextPopup(){ // 교육용 팝업 확인 후 권한 팝업을 띄우는 함수
         AlertDialog.Builder(this)
             .setTitle("권한이 필요합니다.")
@@ -103,9 +148,5 @@ class MainActivity : AppCompatActivity() {
             .setNegativeButton("취소하기") { _, _ -> }
             .create()
             .show()
-    }
-
-    private fun initStartPhotoFrameModeButton(){
-
     }
 }
---
+++ app/src/main/java/com/example/digitalpictureframe/PhotoFrameActivity.kt
@@ -0,0 +1,62 @@
+package com.example.digitalpictureframe
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.timer
+
+class PhotoFrameActivity: AppCompatActivity() {
+
+    private val photoList = mutableListOf<Uri>()
+
+    private var currentPosition = 0 // 현재 보여지고 있는 사진의 position값을 저장하는 변수
+
+    private val photoImageView : ImageView by lazy{
+        findViewById(R.id.photoImageView)
+    }
+
+    private val backgroundPhotoImageView : ImageView by lazy{
+        findViewById(R.id.backgroundPhotoImageView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photoframe)
+
+        getPhotoUriFromIntent()
+
+        startTimer()
+    }
+
+    private fun getPhotoUriFromIntent(){ // intent를 통해 넘겨진 String을 Uri로 변환하여 리스트에 저장하는 함수
+        val size = intent.getIntExtra("photoListSize", 0)
+        for (i in 0..size){
+            intent.getStringExtra("photo$i")?.let{
+                photoList.add(Uri.parse(it))
+            }
+        }
+    }
+
+    private fun startTimer(){
+        timer(period = 5000){
+            runOnUiThread{ // timer 블록 내부는 main Thread에서 동작하는 것이 아니기 때문에 Ui 조작을 위해서는 runOnUiThread() 함수를 이용해야함
+                val current = currentPosition
+                val next = if(photoList.size <= currentPosition + 1) 0 else currentPosition + 1
+
+                backgroundPhotoImageView.setImageURI(photoList[current])
+
+                photoImageView.alpha = 0f // alpha는 투명도를 의미
+                photoImageView.setImageURI(photoList[next])
+                photoImageView.animate() // View에 애니메이션 효과를 주기 위한 함수
+                    .alpha(1.0f)
+                    .setDuration(1000)
+                    .start()
+
+                currentPosition = next
+            }
+        }
+    }
+
+}
